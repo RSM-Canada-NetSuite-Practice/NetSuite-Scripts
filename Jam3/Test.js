09db98b0da74ca7341a38388da8792fb9077baf3
Bean <Nicolas.Bean@rsmcanada.com>
2020-02-20 13:20:41 -0500

    Test2

---
+++ Jam3/Test.js
@@ -0,0 +1,15 @@
+/*******************************************************************
+*
+*
+* Name: Test.js
+* Script Type: MapReduceScript
+* Version: 0.0.1
+*
+*
+* Author: Author Name
+* Purpose: A description of the purpose of this script
+* Script: The script record id
+* Deploy: The script deployment record id 
+*
+*
+* ******************************************************************* */
